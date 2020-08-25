f61c979e1e39079d3bd2ea0574501ee5aa371ae0
Sergei Golds <sergei.golds@gmail.com>
2020-08-25 13:17:51 +0300

    admin user

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -3,17 +3,41 @@
 namespace app\controllers\admin;
 
 use app\models\User;
+use ishop\libs\Pagination;
 
 class UserController extends AppController
 {
+
+    public function indexAction()
+    {
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = 3;
+        $count = \R::count('user');
+        $pagination = new Pagination($page, $perpage, $count);
+        $start = $pagination->getStart();
+        $users = \R::findAll('user', "LIMIT $start, $perpage");
+        $this->setMeta('Список пользователей');
+        $this->set(compact('users', 'pagination', 'count'));
+    }
+
+    public function editAction()
+    {
+        $user_id = $this->getRequestID();
+        $user = \R::load('user', $user_id);
+
+        $orders = \R::getAll("SELECT `order`.`id`, `order`.`user_id`, `order`.`status`, `order`.`date`, `order`.`update_at`, `order`.`currency`, ROUND(SUM(`order_product`.`price`), 2) AS `sum` FROM `order`
+  JOIN `order_product` ON `order`.`id` = `order_product`.`order_id`
+  WHERE user_id = {$user_id} GROUP BY `order`.`id` ORDER BY `order`.`status`, `order`.`id`");
+
+        $this->setMeta('Редактирование профиля пользователя');
+        $this->set(compact('user', 'orders'));
+    }
 
     public function loginAdminAction()
     {
         if (!empty($_POST)) {
             $user = new User();
-            if ($user->login(true)) {
-                $_SESSION['success'] = 'Вы успешно авторизованы';
-            } else {
+            if (!$user->login(true)) {
                 $_SESSION['error'] = 'Логин/пароль введены неверно';
             }
             if (User::isAdmin()) {
