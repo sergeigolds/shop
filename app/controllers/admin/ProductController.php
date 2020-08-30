71fe4e96dff8fe3319d4c04bd052d0a7e11029db
Sergei Golds <sergei.golds@gmail.com>
2020-08-30 14:12:09 +0300

    admin edit products

--- app/controllers/admin/ProductController.php
+++ app/controllers/admin/ProductController.php
@@ -7,9 +7,11 @@ use app\models\AppModel;
 use ishop\App;
 use ishop\libs\Pagination;
 
-class ProductController extends AppController {
+class ProductController extends AppController
+{
 
-    public function indexAction(){
+    public function indexAction()
+    {
         $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
         $perpage = 10;
         $count = \R::count('product');
@@ -20,12 +22,13 @@ class ProductController extends AppController {
         $this->set(compact('products', 'pagination', 'count'));
     }
 
-    public function addImageAction(){
-        if(isset($_GET['upload'])){
-            if($_POST['name'] == 'single'){
+    public function addImageAction()
+    {
+        if (isset($_GET['upload'])) {
+            if ($_POST['name'] == 'single') {
                 $wmax = App::$app->getProperty('img_width');
                 $hmax = App::$app->getProperty('img_height');
-            }else{
+            } else {
                 $wmax = App::$app->getProperty('gallery_width');
                 $hmax = App::$app->getProperty('gallery_height');
             }
@@ -35,8 +38,46 @@ class ProductController extends AppController {
         }
     }
 
-    public function addAction(){
-        if(!empty($_POST)){
+    public function editAction()
+    {
+        if (!empty($_POST)) {
+            $id = $this->getRequestID(false);
+            $product = new Product();
+            $data = $_POST;
+            $product->load($data);
+            $product->attributes['status'] = $product->attributes['status'] ? '1' : '0';
+            $product->attributes['hit'] = $product->attributes['hit'] ? '1' : '0';
+            $product->getImg();
+            if (!$product->validate($data)) {
+                $product->getErrors();
+                redirect();
+            }
+            if ($product->update('product', $id)) {
+                $product->editFilter($id, $data);
+                $product->editRelatedProduct($id, $data);
+                $product->saveGallery($id);
+                $alias = AppModel::createAlias('product', 'alias', $data['title'], $id);
+                $product = \R::load('product', $id);
+                $product->alias = $alias;
+                \R::store($product);
+                $_SESSION['success'] = 'Изменения сохранены';
+                redirect();
+            }
+        }
+
+        $id = $this->getRequestID();
+        $product = \R::load('product', $id);
+        App::$app->setProperty('parent_id', $product->category_id);
+        $filter = \R::getCol('SELECT attr_id FROM attribute_product WHERE product_id = ?', [$id]);
+        $related_product = \R::getAll("SELECT related_product.related_id, product.title FROM related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ?", [$id]);
+        $gallery = \R::getCol('SELECT img FROM gallery WHERE product_id = ?', [$id]);
+        $this->setMeta("Редактирование товара {$product->title}");
+        $this->set(compact('product', 'filter', 'related_product', 'gallery'));
+    }
+
+    public function addAction()
+    {
+        if (!empty($_POST)) {
             $product = new Product();
             $data = $_POST;
             $product->load($data);
@@ -44,13 +85,13 @@ class ProductController extends AppController {
             $product->attributes['hit'] = $product->attributes['hit'] ? '1' : '0';
             $product->getImg();
 
-            if(!$product->validate($data)){
+            if (!$product->validate($data)) {
                 $product->getErrors();
                 $_SESSION['form_data'] = $data;
                 redirect();
             }
 
-            if($id = $product->save('product')){
+            if ($id = $product->save('product')) {
                 $product->saveGallery($id);
                 $alias = AppModel::createAlias('product', 'alias', $data['title'], $id);
                 $p = \R::load('product', $id);
@@ -66,13 +107,14 @@ class ProductController extends AppController {
         $this->setMeta('Новый товар');
     }
 
-    public function relatedProductAction(){
+    public function relatedProductAction()
+    {
         $q = isset($_GET['q']) ? $_GET['q'] : '';
         $data['items'] = [];
         $products = \R::getAssoc('SELECT id, title FROM product WHERE title LIKE ? LIMIT 10', ["%{$q}%"]);
-        if($products){
+        if ($products) {
             $i = 0;
-            foreach($products as $id => $title){
+            foreach ($products as $id => $title) {
                 $data['items'][$i]['id'] = $id;
                 $data['items'][$i]['text'] = $title;
                 $i++;
@@ -80,6 +122,20 @@ class ProductController extends AppController {
         }
         echo json_encode($data);
         die;
+    }
+
+    public function deleteGalleryAction()
+    {
+        $id = isset($_POST['id']) ? $_POST['id'] : null;
+        $src = isset($_POST['src']) ? $_POST['src'] : null;
+        if (!$id || !$src) {
+            return;
+        }
+        if (\R::exec("DELETE FROM gallery WHERE product_id = ? AND img = ?", [$id, $src])) {
+            @unlink(WWW . "/images/$src");
+            exit('1');
+        }
+        return;
     }
 
 }
--- public/adminlte/my.js
+++ public/adminlte/my.js
@@ -32,6 +32,36 @@ $('.delete').click(function () {
     if (!res) return false;
 });
 
+$('.del-item').on('click', function () {
+    var res = confirm('Подтвердите действие');
+    if (!res) return false;
+    var $this = $(this),
+        id = $this.data('id'),
+        src = $this.data('src');
+    $.ajax({
+        url: adminpath + '/product/delete-gallery',
+        data: {id: id, src: src},
+        type: 'POST',
+        beforeSend: function () {
+            $this.closest('.file-upload').find('.overlay').css({'display': 'block'});
+        },
+        success: function (res) {
+            setTimeout(function () {
+                $this.closest('.file-upload').find('.overlay').css({'display': 'none'});
+                if (res == 1) {
+                    $this.fadeOut();
+                }
+            }, 1000);
+        },
+        error: function () {
+            setTimeout(function () {
+                $this.closest('.file-upload').find('.overlay').css({'display': 'none'});
+                alert('Ошибка');
+            }, 1000);
+        }
+    });
+});
+
 $('.main-sidebar  a').each(function () {
     var location = window.location.protocol + '//' + window.location.host + window.location.pathname;
     var link = this.href;
@@ -43,48 +73,56 @@ $('.main-sidebar  a').each(function () {
     }
 });
 
-var buttonSingle = $("#single"),
-    buttonMulti = $("#multi"),
-    file;
-new AjaxUpload(buttonSingle, {
-    action: adminpath + buttonSingle.data('url') + "?upload=1",
-    data: {name: buttonSingle.data('name')},
-    name: buttonSingle.data('name'),
-    onSubmit: function (file, ext) {
-        if (!(ext && /^(jpg|png|jpeg|gif)$/i.test(ext))) {
-            alert('Ошибка! Разрешены только картинки');
-            return false;
+if ($('div').is('#single')) {
+    var buttonSingle = $("#single"),
+        buttonMulti = $("#multi"),
+        file;
+}
+
+if (buttonSingle) {
+    new AjaxUpload(buttonSingle, {
+        action: adminpath + buttonSingle.data('url') + "?upload=1",
+        data: {name: buttonSingle.data('name')},
+        name: buttonSingle.data('name'),
+        onSubmit: function (file, ext) {
+            if (!(ext && /^(jpg|png|jpeg|gif)$/i.test(ext))) {
+                alert('Ошибка! Разрешены только картинки');
+                return false;
+            }
+            buttonSingle.closest('.file-upload').find('.overlay').css({'display': 'block'});
+
+        },
+        onComplete: function (file, response) {
+            setTimeout(function () {
+                buttonSingle.closest('.file-upload').find('.overlay').css({'display': 'none'});
+
+                response = JSON.parse(response);
+                $('.' + buttonSingle.data('name')).html('<img src="/images/' + response.file + '" style="max-height: 150px;">');
+            }, 1000);
         }
-        buttonSingle.closest('.file-upload').find('.overlay').css({'display': 'block'});
+    });
+}
 
-    },
-    onComplete: function (file, response) {
-        setTimeout(function () {
-            buttonSingle.closest('.file-upload').find('.overlay').css({'display': 'none'});
-            response = JSON.parse(response);
-            $('.' + buttonSingle.data('name')).html('<img src="/images/' + response.file + '" style="max-height: 150px;">');
-        }, 1000);
-    }
-});
+if (buttonMulti) {
+    new AjaxUpload(buttonMulti, {
+        action: adminpath + buttonMulti.data('url') + "?upload=1",
+        data: {name: buttonMulti.data('name')},
+        name: buttonMulti.data('name'),
+        onSubmit: function (file, ext) {
+            if (!(ext && /^(jpg|png|jpeg|gif)$/i.test(ext))) {
+                alert('Ошибка! Разрешены только картинки');
+                return false;
+            }
+            buttonMulti.closest('.file-upload').find('.overlay').css({'display': 'block'});
 
-new AjaxUpload(buttonMulti, {
-    action: adminpath + buttonMulti.data('url') + "?upload=1",
-    data: {name: buttonMulti.data('name')},
-    name: buttonMulti.data('name'),
-    onSubmit: function (file, ext) {
-        if (!(ext && /^(jpg|png|jpeg|gif)$/i.test(ext))) {
-            alert('Ошибка! Разрешены только картинки');
-            return false;
+        },
+        onComplete: function (file, response) {
+            setTimeout(function () {
+                buttonMulti.closest('.file-upload').find('.overlay').css({'display': 'none'});
+
+                response = JSON.parse(response);
+                $('.' + buttonMulti.data('name')).append('<img src="/images/' + response.file + '" style="max-height: 150px;">');
+            }, 1000);
         }
-        buttonMulti.closest('.file-upload').find('.overlay').css({'display': 'block'});
-
-    },
-    onComplete: function (file, response) {
-        setTimeout(function () {
-            buttonMulti.closest('.file-upload').find('.overlay').css({'display': 'none'});
-
-            response = JSON.parse(response);
-            $('.' + buttonMulti.data('name')).append('<img src="/images/' + response.file + '" style="max-height: 150px;">');
-        }, 1000);
-    }
-});
+    });
+}
